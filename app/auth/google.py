6d5621169baba06dfab9a73315ef3b43b4a47d49
Ansel Rognlie <36503186+anselrognlie@users.noreply.github.com>
2021-07-30 08:50:38 -0700

    uses proxies for deferred lookups

--- app/auth/google.py
+++ app/auth/google.py
@@ -1,25 +1,19 @@
 import os
 from authlib.integrations.flask_client import OAuth
+from werkzeug.local import Local
 
-GOOGLE_CLIENT_ID = None
-GOOGLE_CLIENT_SECRET = None
-GOOGLE_DISCOVERY_URL = None
-GOOGLE_CONF_URL = None
-oauth = None
+_l = Local()
+GOOGLE_CLIENT_ID = _l('GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SECRET = _l('GOOGLE_CLIENT_SECRET')
+GOOGLE_DISCOVERY_URL = _l('GOOGLE_DISCOVERY_URL')
+oauth = _l('oauth')
 
 def load(app):
-    global GOOGLE_CLIENT_ID
-    global GOOGLE_CLIENT_SECRET
-    global GOOGLE_DISCOVERY_URL
-    global GOOGLE_CONF_URL
-    global oauth
-
     GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
     app.config['GOOGLE_CLIENT_ID'] = GOOGLE_CLIENT_ID 
     GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
     app.config['GOOGLE_CLIENT_SECRET'] = GOOGLE_CLIENT_SECRET
     GOOGLE_DISCOVERY_URL = os.environ.get("GOOGLE_DISCOVERY_URL", None)
-    # GOOGLE_CONF_URL = os.environ.get("GOOGLE_CONF_URL", None)
 
     oauth = OAuth(app)
     oauth.register(
@@ -29,3 +23,8 @@ def load(app):
             'scope': 'openid email profile'
         }
     )
+
+    _l.GOOGLE_CLIENT_ID = GOOGLE_CLIENT_ID
+    _l.GOOGLE_CLIENT_SECRET = GOOGLE_CLIENT_SECRET
+    _l.GOOGLE_DISCOVERY_URL = GOOGLE_DISCOVERY_URL
+    _l.oauth = oauth
